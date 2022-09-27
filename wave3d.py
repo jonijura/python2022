22e4a52f826861f5ccc90f513f516c4c614c14af
Räty <jonijura@jyu.fi>
2022-09-27 13:45:54 +0300

    performance measurements

--- wave3d.py
+++ wave3d.py
@@ -10,31 +10,50 @@ from scipy.sparse import diags
 import matplotlib.pyplot as plt
 from gmesh_recmesh import makecube
 from pydec import simplicial_complex
+import time
 
-Nxyz = 5
+Nxyz = 13
 bxyz = np.pi
 dxyz = bxyz/Nxyz
 
 bt = 6
-Nt = 50
+Nt = 200
 dt = bt/Nt
 
+prev = time.time()
+print("making mesh")
 V,E = makecube(bxyz,bxyz,bxyz,dxyz)
+print("\tmesh complete in "+str(time.time()-prev))
+prev = time.time()
+print("making complex")
 sc = simplicial_complex(V,E)
+print("\tcomplex complete in "+str(time.time()-prev))
 
+print("calculating operations")
+prev = time.time()
 d0=sc[0].d
+print("\td0 calculated in "+str(time.time()-prev))
+prev=time.time()
 h1=sc[1].star
+print("\th1 calculated in "+str(time.time()-prev))
+prev=time.time()
 h0i=sc[0].star_inv
+print("\th0i calculated in "+str(time.time()-prev))
 #some mesh quality statistics
-print("h1 min: "+str(np.min(h1.diagonal())))
-print("h1 max: "+str(np.max(h1.diagonal())))
-print("h0i min: "+str(np.min(h0i.diagonal())))
-print("h0i max: "+str(np.max(h0i.diagonal())))
+# print("h1 min: "+str(np.min(h1.diagonal())))
+# print("h1 max: "+str(np.max(h1.diagonal())))
+# print("h0i min: "+str(np.min(h0i.diagonal())))
+# print("h0i max: "+str(np.max(h0i.diagonal())))
+
 #set boundary condition f=0
+prev = time.time()
+print("finding boundary nodes")
 boundFcs = sc.boundary()
 boundEdgs = set([boundEdg for face in boundFcs for boundEdg in face.boundary()])
 boundVerts = set([boundVert for edge in boundEdgs for boundVert in edge.boundary()])
 boundVertInds = [sc[0].simplex_to_index[v] for v in boundVerts]
+print("found boundary nodes in "+str(time.time()-prev))
+
 h0ivals = h0i.diagonal()
 h0ivals[boundVertInds]=0
 h0i = diags(h0ivals)
@@ -53,10 +72,14 @@ refp = len(V)-3
 
 hist = np.zeros(Nt+1)
 hist[0]=f[refp]
+
+prev=time.time()
+print("timestepping")
 for i in range(Nt):
     f+=B*g
     g+=A*f
     hist[i+1]=f[refp]
+print("timestepping complete in "+str(time.time()-prev))
 
 lp = np.linspace(0,bt, Nt+1)
 plt.title("History of reference point")
