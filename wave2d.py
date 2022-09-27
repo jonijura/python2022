1237f265c2618aece9f4d016c8c0e0bcb6fb19d5
Räty <jonijura@jyu.fi>
2022-09-27 11:37:12 +0300

    refaktorointia2

--- wave2d.py
+++ wave2d.py
@@ -38,12 +38,14 @@ dt = bt/Nt
 V,E = makerec(bxy,bxy,dxy)
 sc = simplicial_complex(V,E)
 
-f = np.zeros(sc[1].num_simplices)
-g = np.zeros(sc[0].num_simplices)
-
 d0=sc[0].d
 h1=sc[1].star
 h0i=sc[0].star_inv
+#some mesh quality statistics
+print("h1 min: "+str(np.min(h1.diagonal())))
+print("h1 max: "+str(np.max(h1.diagonal())))
+print("h0i min: "+str(np.min(h0i.diagonal())))
+print("h0i max: "+str(np.max(h0i.diagonal())))
 #set boundary condition f=0
 boundEdgs = sc.boundary()
 boundVerts = set([boundVert for edge in boundEdgs for boundVert in edge.boundary()])
@@ -51,28 +53,27 @@ boundVertInds = [sc[0].simplex_to_index[v] for v in boundVerts]
 h0ivals = h0i.diagonal()
 h0ivals[boundVertInds]=0
 h0i = diags(h0ivals)
-#some mesh quality statistics
-print("h1 min: "+str(np.min(h1.diagonal())))
-print("h1 max: "+str(np.max(h1.diagonal())))
-print("h0i min: "+str(np.min(h0i.diagonal())))
-print("h0i max: "+str(np.max(h0i.diagonal())))
-#initial values
-g = np.sin(V[:,0])*np.sin(2*V[:,1])
+
+g = np.zeros(sc[1].num_simplices)
+f = np.zeros(sc[0].num_simplices)
 
 A = dt*h1*d0
 B = dt*h0i*(-d0.T)
 
+#initial values f(0), g(dt/2)
+f = np.sin(V[:,0])*np.sin(2*V[:,1])
+g = 0.5*A*f
 
-f=0.5*A*g
 for i in range(Nt):
-    g+=B*f
-    f+=A*g
-    ax = plt.axes(projection='3d')
-    ax.set_zlim( [-1,1])
-    plt.title("Simulaatio")
-    ax.plot_trisurf(V[:,0], V[:,1], g,
-                    cmap='viridis', edgecolor='none')
-    plt.show()
+    f+=B*g
+    g+=A*f
+
+ax = plt.axes(projection='3d')
+ax.set_zlim( [-1,1])
+plt.title("Result")
+ax.plot_trisurf(V[:,0], V[:,1], f,
+                cmap='viridis', edgecolor='none')
+plt.show()
     
 
 
