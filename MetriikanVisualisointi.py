c63abe902a06ce508178e9f1c898470f4630f59a
Räty <jonijura@jyu.fi>
2022-10-27 13:29:29 +0300

    päivitys

--- MetriikanVisualisointi.py
+++ MetriikanVisualisointi.py
@@ -9,12 +9,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-N = 20
-lx = np.linspace(-1,1,N)
-lt = np.linspace(-1, 1,N)
-lx,lt = np.meshgrid(lx,lt);
-d = lt**2-lx**2
+# N = 20
+# lx = np.linspace(-1,1,N)
+# lt = np.linspace(-1, 1,N)
+# lx,lt = np.meshgrid(lx,lt);
+# d = lt**2-lx**2
 
-fig = plt.figure(figsize=(10,10), dpi=80)
-levels=np.sign(np.linspace(-1,1,15))*np.linspace(-1,1,15)**2
-plt.contour(lx,lt, d, levels=levels)
+# fig = plt.figure(figsize=(10,10), dpi=80)
+# levels=np.sign(np.linspace(-1,1,15))*np.linspace(-1,1,15)**2
+# plt.contour(lx,lt, d, levels=levels)
+
+
+from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial import Delaunay
+from gmesh_recmesh import reqrecMsh
+
+points = np.random.rand(20,2)
+V,E = reqrecMsh(1,1,0.4)
+# points = V
+
+vor = Voronoi(points)
+plt.figure(figsize=(10,10), dpi=80)
+voronoi_plot_2d(vor, plt.axes())
+
+# tri = Delaunay(points)
+# plt.triplot(points[:,0], points[:,1], tri.simplices)
+# plt.plot(points[:,0], points[:,1], 'o')
+plt.show()
+
+vert = [0<a[0] and a[0]<1 and 0<a[1] and a[1]<1 for a in vor.vertices]
+vor = Voronoi(vor.vertices[vert])
+plt.figure(figsize=(10,10), dpi=80)
+voronoi_plot_2d(vor, plt.axes())
--- cpptulostenTarkastelu.py
+++ cpptulostenTarkastelu.py
@@ -9,14 +9,43 @@ Created on Thu Oct 13 12:06:48 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-V = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\vert.txt")
-E = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\tria.txt",dtype='int32')
-f = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\solutions.txt")
+# V = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\vert.txt")
+# E = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\tria.txt",dtype='int32')
+# f = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\solutions.txt")
 
-plt.plot(f)
+# plt.plot(f)
+# plt.show()
 
 # fig = plt.figure()
 # ax = fig.add_subplot(121,projection='3d')
 # ax.set_zlim( [-1,1])
 # plt.title("Simulation result")
 # ax.plot_trisurf(V[:,0], V[:,1], f,cmap='viridis', edgecolor='none')
+
+# -*- coding: utf-8 -*-
+
+
+
+solGlob = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\globalTimestepEnergy.txt")
+solLoc1 = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\localTimestepEnergy.txt")
+
+plt.plot(np.linspace(0,1,solGlob.size),solGlob)
+plt.plot(np.linspace(0,1,solLoc1.size),solLoc1)
+plt.legend(["global timestep", "local timestep1"])
+
+# from scipy.sparse import csr_matrix
+# from scipy.sparse.linalg import eigs
+# mat = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\SystemMatrix.txt")
+# x = mat[2].astype(np.int32)
+# y = mat[1].astype(np.int32)
+# size = max(x)+1
+# matcsr = csr_matrix((mat[0], (x,y)), shape=(size,size))
+# val, vec = eigs(matcsr,1)
+# print(f"eigenvalue: {abs(val)[0]}")
+
+# dt = 0.000384441
+# i = np.arange(20000)
+# fm = 5.0;
+# lev = 20/(np.pi*fm);
+# pl = np.exp(-(i*dt-3*lev)*(i*dt-3*lev)/(lev*lev))*np.sin(2*np.pi*fm*(i*dt-3*lev));
+# plt.plot(pl)
