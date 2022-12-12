6d296dd9cd81afb37add97b6b55af045733b970a
Räty <jonijura@jyu.fi>
2022-12-12 15:23:09 +0200

    testejä

--- cpptulostenTarkastelu.py
+++ cpptulostenTarkastelu.py
@@ -3,84 +3,18 @@
 Created on Thu Oct 13 12:06:48 2022
 
 @author: jonijura
+
+jukan kirjastolla tuotettujen tuloksien analysointi 2+1 ja 1+1 aika-paikka verkko simuloinneille
 """
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# V = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\vert.txt")
-# E = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\tria.txt",dtype='int32')
-# f = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\solutions.txt")
+resultFolder = "C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\"
 
-# plt.plot(f)
-# plt.show()
-
-# fig = plt.figure()
-# ax = fig.add_subplot(121,projection='3d')
-# ax.set_zlim( [-1,1])
-# plt.title("Simulation result")
-# ax.plot_trisurf(V[:,0], V[:,1], f,cmap='viridis', edgecolor='none')
-
-# -*- coding: utf-8 -*-
-
-
-
-solGlob = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\globalTimestepEnergy.txt")
-solLoc1 = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\localTimestepEnergy.txt")
-
-plt.plot(np.linspace(0,1,solGlob.size),solGlob)
-plt.plot(np.linspace(0,1,solLoc1.size),solLoc1)
-plt.legend(["global timestep", "local timestep1"])
-plt.show()
-
-# from scipy.sparse import csr_matrix
-# from scipy.sparse.linalg import eigs
-# mat = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\SystemMatrix.txt")
-# x = mat[2].astype(np.int32)
-# y = mat[1].astype(np.int32)
-# size = max(x)+1
-# matcsr = csr_matrix((mat[0], (x,y)), shape=(size,size))
-# val, vec = eigs(matcsr,1)
-# print(f"eigenvalue: {abs(val)[0]}")
-
-# dt = 0.000384441
-# i = np.arange(20000)
-# fm = 5.0;
-# lev = 20/(np.pi*fm);
-# pl = np.exp(-(i*dt-3*lev)*(i*dt-3*lev)/(lev*lev))*np.sin(2*np.pi*fm*(i*dt-3*lev));
-# plt.plot(pl)
-
-#Fourier analyysi
-refphist = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\GTSrefphistory.txt")
-fmin=0.01
-fmax=1.5
-dt = 0.000224792
-a = 1.0
-N=refphist.shape[0]
-
-f = (1/dt)*np.linspace(0,1,N) #Diskreettiä muunnosta vastaavat taajuudet
-st = int(np.round(fmin*dt*N))
-end = int(np.round(fmax*dt*N))
-
-egmds = np.sqrt(np.array((3,6,9,11,12,14,19)))/(2*a)
-
-p = abs(np.fft.fft(refphist[:,0]))**2
-plt.plot(f[st:end],p[st:end])
-# plt.show()
-p = abs(np.fft.fft(refphist[:,1]))**2
-plt.plot(f[st:end],p[st:end])
-# plt.show()
-p = abs(np.fft.fft(refphist[:,2]))**2
-plt.plot(f[st:end],p[st:end])
-# plt.plot(egmds, np.ones(egmds.size),'r.')
-plt.show()
-
-aoi = p[st:end]
-foi = f[np.argwhere(aoi>1e7)]
-
-
-tsmsh = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\tsmsh.txt")
+#1+1 interpolated 1-form
+tsmsh = np.loadtxt(resultFolder+"conv\\16_interpolated.txt")
 fig = plt.figure(figsize=(8, 8), dpi=120)
 ax = fig.add_subplot(221,projection='3d')
 ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], tsmsh[:,2],cmap='viridis', edgecolor='none')
@@ -93,18 +27,17 @@ ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], tsmsh[:,3],cmap='viridis', edgecolor='no
 ax.set_xlabel('$x$')
 ax.set_ylabel('$t$')
 ax.set_zlabel('$F_t$')
-qf = 1
 
-ax = fig.add_subplot(223,projection='3d')
 e1 = np.sin(tsmsh[:,0]) * np.cos(tsmsh[:,1])
-ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e1-qf * tsmsh[:,2] ,cmap='viridis', edgecolor='none')
+ax = fig.add_subplot(223,projection='3d')
+ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e1- tsmsh[:,2] ,cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
 ax.set_ylabel('$t$')
 ax.set_zlabel('$\delta F_x$')
 
 e2 = np.cos(tsmsh[:,0]) * np.sin(tsmsh[:,1])
 ax = fig.add_subplot(224,projection='3d')
-ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e2-qf * tsmsh[:,3] ,cmap='viridis', edgecolor='none')
+ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e2- tsmsh[:,3] ,cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
 ax.set_ylabel('$t$')
 ax.set_zlabel('$\delta F_t$')
@@ -112,78 +45,169 @@ plt.show()
 
 
 
-tsmsh = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\tsmsh2.txt")
+#1+1 without interpolation as differential form (visualised at edge centers)
+tsmsh2 = np.loadtxt(resultFolder+"conv\\16_1+1form.txt")
+sol = np.cos(tsmsh2[:,0]) * np.cos(tsmsh2[:,1]) - np.cos(tsmsh2[:,2]) * np.cos(tsmsh2[:,3])
+h1 = tsmsh2[:,0:2]-tsmsh2[:,2:4]
+h1 = np.sum(np.abs(h1)**2,axis=-1)**(1./2)
+diff = sol-tsmsh2[:,4]
+print((diff*diff).mean())
+
+ccs = tsmsh2[:,2:4] + 0.5*(tsmsh2[:,0:2]-tsmsh2[:,2:4])
+fig = plt.figure(figsize=(12, 6), dpi=120)
+ax = fig.add_subplot(121,projection='3d')
+ax.plot_trisurf(ccs[:,0], ccs[:,1], diff*h1*diff,cmap='viridis', edgecolor='none')
+ax.set_xlabel('$x$')
+ax.set_ylabel('$t$')
+ax.title.set_text("sqr diff hodge normed")
+
+ax = fig.add_subplot(122,projection='3d')
+ax.plot_trisurf(ccs[:,0], ccs[:,1], diff*diff,cmap='viridis', edgecolor='none')
+ax.set_xlabel('$x$')
+ax.set_ylabel('$t$')
+ax.title.set_text("sqr diff")
+plt.show()
+
+
+#2+1 form with interpolation, dont forget to update reference time
+tsmsh = np.loadtxt(resultFolder+"2+1interpolated.txt")
+#reference solution, to compare to exact vector field solution, replace commented lines
+exact_interpolated = np.loadtxt(resultFolder+"2+1exact_solution_interpolated.txt") 
+ref_time = np.pi
+
 fig = plt.figure(figsize=(11, 8), dpi=120)
 ax = fig.add_subplot(231,projection='3d')
 ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], tsmsh[:,2],cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
-ax.set_ylabel('$t$')
+ax.set_ylabel('$y$')
 ax.set_zlabel('$F_x$')
 
 ax = fig.add_subplot(232,projection='3d')
 ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], tsmsh[:,3],cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
-ax.set_ylabel('$t$')
+ax.set_ylabel('$y$')
 ax.set_zlabel('$F_y$')
 
 ax = fig.add_subplot(233,projection='3d')
 ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], tsmsh[:,4],cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
-ax.set_ylabel('$t$')
+ax.set_ylabel('$y$')
 ax.set_zlabel('$F_t$')
 
-qf = 1
-tmax = np.pi/2
-
+e1 = exact_interpolated[:,2]#1/np.sqrt(2) * np.cos(tsmsh[:,0]) * np.sin(tsmsh[:,1]) * np.sin(np.sqrt(2)*ref_time)
 ax = fig.add_subplot(234,projection='3d')
-e1 = 1/np.sqrt(2) * np.cos(tsmsh[:,0]) * np.sin(tsmsh[:,1]) * np.sin(np.sqrt(2)*tmax)
-ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e1-qf * tsmsh[:,2] ,cmap='viridis', edgecolor='none')
+ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e1 - tsmsh[:,2] ,cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
-ax.set_ylabel('$t$')
+ax.set_ylabel('$y$')
 ax.set_zlabel('$\delta F_x$')
 
 
-e2 = 1/np.sqrt(2) * np.sin(tsmsh[:,0]) * np.cos(tsmsh[:,1]) * np.sin(np.sqrt(2)*tmax)
+e2 = exact_interpolated[:,3]#1/np.sqrt(2) * np.sin(tsmsh[:,0]) * np.cos(tsmsh[:,1]) * np.sin(np.sqrt(2)*ref_time)
 ax = fig.add_subplot(235,projection='3d')
-ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e2-qf * tsmsh[:,3] ,cmap='viridis', edgecolor='none')
+ax.plot_trisurf(tsmsh[:,0], tsmsh[:,1], e2 - tsmsh[:,3] ,cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
-ax.set_ylabel('$t$')
+ax.set_ylabel('$y$')
 ax.set_zlabel('$\delta F_y$')
 
 
-e3 = np.sin(tsmsh[:,0]) * np.sin(tsmsh[:,1]) * np.cos(np.sqrt(2)*tmax)
+e3 = exact_interpolated[:,4]#np.sin(tsmsh[:,0]) * np.sin(tsmsh[:,1]) * np.cos(np.sqrt(2)*ref_time)
 ax = fig.add_subplot(236,projection='3d')
-ax.plot_trisurf(tsmsh[:,0],tsmsh[:,1],  e3-qf * tsmsh[:,4],cmap='viridis', edgecolor='none')
+ax.plot_trisurf(tsmsh[:,0],tsmsh[:,1],  e3 - tsmsh[:,4],cmap='viridis', edgecolor='none')
 ax.set_xlabel('$x$')
-ax.set_ylabel('$t$')
+ax.set_ylabel('$y$')
 ax.set_zlabel('$\delta F_t$')
 
 plt.show()
 
- 
-print(1/np.max(abs(e1))/np.max(abs(tsmsh[:,2])))
-print(1/np.max(abs(e2))/np.max(abs(tsmsh[:,3])))
-print(1/np.max(abs(e3))/np.max(abs(tsmsh[:,4])))
 
 
+#2+1 simulation values without interpolation
+from scipy.stats import binned_statistic
+noip = np.loadtxt(resultFolder+"2+1nointerpolation.txt")
+sq2 = np.sqrt(2)
+exact = -(np.sin(noip[:,0]) * np.sin(noip[:,1]) * np.sin(sq2*noip[:,2]) 
+        - np.sin(noip[:,3]) * np.sin(noip[:,4]) * np.sin(sq2*noip[:,5]))/sq2
+diff = exact - noip[:,6]
+h1 = noip[:,0:3]-noip[:,3:6]
+h1 = np.sum(np.abs(h1)**2,axis=-1)**(1./2)
+aproxt = (noip[:,2]+noip[:,5])/2
 
-
-
-freg2 = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\freq2.txt")
-
-plt.plot(freg2[:,1],freg2[:,0])
-plt.plot(freg2[:,1], (1/np.sqrt(2) * np.sin(np.sqrt(2)*freg2[:,1])))
-plt.plot(freg2[:,1], freg2[:,0]-(1/np.sqrt(2) * np.sin(np.sqrt(2)*freg2[:,1])))
-
-
-
-
-freg = np.loadtxt("C:\MyTemp\cpp\Samples\MeshGeneration2\\build\\freq.txt")
-
-plt.plot(freg[:,1],freg[:,0])
-plt.plot(freg[:,1], np.cos(np.sqrt(2)*freg[:,1]))
-plt.plot(freg[:,1], freg[:,0]-np.cos(np.sqrt(2)*freg[:,1]))
+binnum = 15
+mean_stat = binned_statistic(aproxt, diff*diff, 
+                             statistic='mean', 
+                             bins=binnum , 
+                             range=(0, np.pi))
+plt.plot(aproxt, diff*diff, '.')
+plt.title("diff squared")
+plt.plot(np.linspace(0, np.pi, binnum ), 10*mean_stat.statistic)
 plt.show()
+mean_stat = binned_statistic(aproxt, diff*h1*diff, 
+                             statistic='mean', 
+                             bins=binnum , 
+                             range=(0, np.pi))
+plt.plot(aproxt, diff*h1*diff, '.')
+plt.title("diff squared hodge normed")
+plt.plot(np.linspace(0, np.pi, binnum ), 10*mean_stat.statistic)
+plt.show()
+
+
+
+#1+1 convergence test
+arr = [(i+3)**2 for i in range(9)]
+solt = np.zeros((len(arr),3))
+for i,val in enumerate(arr):
+    tsmsh2 = np.loadtxt(resultFolder+"conv\\"+str(val)+"_1+1form.txt")
+    sol = np.cos(tsmsh2[:,0]) * np.cos(tsmsh2[:,1]) - np.cos(tsmsh2[:,2]) * np.cos(tsmsh2[:,3])
+    h1 = tsmsh2[:,0:2]-tsmsh2[:,2:4]
+    h1 = np.sum(np.abs(h1)**2,axis=-1)**(1./2)
+    diff = sol-tsmsh2[:,4]
+    solt[i][0] = max(abs(h1*diff))
+    solt[i][1] = (diff*h1*diff).mean()
+    solt[i][2] = np.dot(diff, h1*diff)
+dx = np.array([0.541509,
+                0.312709,
+                0.202599,
+                0.141636,
+                0.104479,
+                0.080202,
+                0.063483,
+                0.051488,
+                0.042592])
+fig = plt.figure(figsize=(13, 4), dpi=120)
+
+ax = fig.add_subplot(131)
+plt.yscale("log")
+plt.xscale("log")
+ax.plot(dx, solt[:,0])
+ax.plot(dx,1e-2*dx**4)
+ax.set_ylabel("error")
+ax.legend(["maximum error","$O(n^4)$"])
+ax = fig.add_subplot(132)
+plt.yscale("log")
+plt.xscale("log")
+ax.plot(dx, solt[:,1])
+ax.plot(dx,1e-2*dx**7)
+ax.set_xlabel("Longest mesh edge")
+ax.legend(["mean square error","$O(n^7)$"])
+ax = fig.add_subplot(133)
+plt.yscale("log")
+plt.xscale("log")
+ax.plot(dx, solt[:,2])
+ax.plot(dx,1e-2*dx**5)
+ax.legend(["error norm","$O(n^5)$"])
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
